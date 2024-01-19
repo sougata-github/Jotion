40ed80fb69fb86b88e578cce3c42d869d20b78d9
Sougata <sougatadas9874@gmail.com>
2024-01-19 16:12:13 +0530

    fix: heading and footer styles.

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center w-full p-4 bg-background z-50 dark:bg-[#1F1F1F]">
-      <p className="text-sm text-black dark:text-white">&copy; 2024 | Jotion</p>
+    <div className="flex items-center justify-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
+      <p className="text-sm text-black dark:text-white">&copy;Jotion | 2024</p>
     </div>
   );
 };
--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -5,8 +5,8 @@ const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans Unified. Welcome to{" "}
-        <span className="sm:underline">Jotion</span>
+        Your Ideas, Documents, & Plans unified. Welcome <br />
+        to Jotion.
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where
@@ -14,7 +14,7 @@ const Heading = () => {
         better, faster work happens.
       </h3>
       <Button>
-        Enter Jotion <ArrowRightIcon className="h-4 w-4 ml-2" />
+        Get Jotion Free <ArrowRightIcon className="h-4 w-4 ml-2" />
       </Button>
     </div>
   );
