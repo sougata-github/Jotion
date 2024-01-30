b81fa945dfe52217ba5c992ff728059745c85e11
Sougata <sougatadas9874@gmail.com>
2024-01-30 23:40:19 +0530

    fix: search only by title.

--- components/SearchCommand.tsx
+++ components/SearchCommand.tsx
@@ -59,7 +59,7 @@ const SearchCommand = () => {
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}-${document.title}`}
+              value={`${document.title}`}
               title={document.title}
               onSelect={onSelect}
             >
