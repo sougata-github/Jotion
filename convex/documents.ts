8e31e572f25e906541d61b59e088bf0e00e9bbd4
Sougata <sougatadas9874@gmail.com>
2024-02-04 23:30:35 +0530

    remove comment.

--- convex/documents.ts
+++ convex/documents.ts
@@ -94,7 +94,6 @@ export const create = mutation({
 
     const userId = identity.subject;
 
-    //create a document
     const document = await ctx.db.insert("documents", {
       title: args.title,
       parentDocument: args.parentDocument,
