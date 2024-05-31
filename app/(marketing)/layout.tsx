1ecca01d619ad599275d8096323b4ea5e152e5b8
Sougata <sougatadas9874@gmail.com>
2024-05-31 13:48:31 +0530

    fix: hide landing page scrollbar on mobile.

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Navbar from "./_components/Navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full dark:bg-[#1F1F1F]">
       <Navbar />
-      <main className="h-full sm:pt-40 overflow-x-hidden pt-[8rem] custom-scrollbar">
+      <main className="h-full sm:pt-40 overflow-x-hidden pt-[8rem] custom-scrollbar max-sm:scrollbar-hidden">
         {children}
       </main>
     </div>
